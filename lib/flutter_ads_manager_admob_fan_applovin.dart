f432ec339299e15f594779000cc6390c75938289
EasyApk <easyapk@icloud.com>
2023-04-06 15:39:41 +0700

    update

--- lib/flutter_ads_manager_admob_fan_applovin.dart
+++ lib/flutter_ads_manager_admob_fan_applovin.dart
@@ -1,8 +1,212 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_core_ads_manager/callback_ads.dart';
+import 'package:flutter_core_ads_manager/iadsmanager/i_initialize.dart';
+import 'package:flutter_core_ads_manager/iadsmanager/i_rewards.dart';
+import 'package:flutter_core_ads_manager/network_ads.dart';
+import 'package:flutter_core_ads_manager/size_ads.dart';
 
 import 'flutter_ads_manager_admob_fan_applovin_platform_interface.dart';
 
 class FlutterAdsManagerAdmobFanApplovin {
   Future<String?> getPlatformVersion() {
-    return FlutterAdsManagerAdmobFanApplovinPlatform.instance.getPlatformVersion();
+    return FlutterAdsManagerAdmobFanApplovinPlatform.instance
+        .getPlatformVersion();
+  }
+
+  void initialize(
+      BuildContext context,
+      IInitialize iInitialize,
+      NetworkAds primaryAds,
+      String? primaryAppId,
+      NetworkAds? secondaryAds,
+      String? secondaryAppId,
+      NetworkAds? tertiaryAds,
+      String? tertiaryAppId,
+      NetworkAds? quaternaryAds,
+      String? quaternaryAppId) {
+    return FlutterAdsManagerAdmobFanApplovinPlatform.instance.initialize(
+        context,
+        iInitialize,
+        primaryAds,
+        primaryAppId,
+        secondaryAds,
+        secondaryAppId,
+        tertiaryAds,
+        tertiaryAppId,
+        quaternaryAds,
+        quaternaryAppId);
+  }
+
+  void loadGdpr(
+      BuildContext context,
+      bool childDirected,
+      NetworkAds primaryAds,
+      NetworkAds? secondaryAds,
+      NetworkAds? tertiaryAds,
+      NetworkAds? quaternaryAds) {
+    return FlutterAdsManagerAdmobFanApplovinPlatform.instance.loadGdpr(context,
+        childDirected, primaryAds, secondaryAds, tertiaryAds, quaternaryAds);
+  }
+
+  void loadInterstitial(
+      BuildContext context,
+      NetworkAds primaryAds,
+      String adUnitPrimaryId,
+      NetworkAds? secondaryAds,
+      String? adUnitSecondaryId,
+      NetworkAds? tertiaryAds,
+      String? adUnitTertiaryAdsId,
+      NetworkAds? quaternaryAds,
+      String? adUnitQuaternaryId) {
+    return FlutterAdsManagerAdmobFanApplovinPlatform.instance.loadInterstitial(
+        context,
+        primaryAds,
+        adUnitPrimaryId,
+        secondaryAds,
+        adUnitSecondaryId,
+        tertiaryAds,
+        adUnitTertiaryAdsId,
+        quaternaryAds,
+        adUnitQuaternaryId);
+  }
+
+  void loadRewards(
+      BuildContext context,
+      NetworkAds primaryAds,
+      String adUnitPrimaryId,
+      NetworkAds? secondaryAds,
+      String? adUnitSecondaryId,
+      NetworkAds? tertiaryAds,
+      String? adUnitTertiaryAdsId,
+      NetworkAds? quaternaryAds,
+      String? adUnitQuaternaryId) {
+    return FlutterAdsManagerAdmobFanApplovinPlatform.instance.loadRewards(
+        context,
+        primaryAds,
+        adUnitPrimaryId,
+        secondaryAds,
+        adUnitSecondaryId,
+        tertiaryAds,
+        adUnitTertiaryAdsId,
+        quaternaryAds,
+        adUnitQuaternaryId);
+  }
+
+  void setTestDevices(
+      BuildContext context,
+      List<String> testDevices,
+      NetworkAds primaryAds,
+      NetworkAds? secondaryAds,
+      NetworkAds? tertiaryAds,
+      NetworkAds? quaternaryAds) {
+    return FlutterAdsManagerAdmobFanApplovinPlatform.instance.setTestDevices(
+        context,
+        testDevices,
+        primaryAds,
+        secondaryAds,
+        tertiaryAds,
+        quaternaryAds);
+  }
+
+  void showBanner(
+      BuildContext context,
+      SizeBanner sizeBanner,
+      NetworkAds primaryAds,
+      String adUnitPrimaryId,
+      NetworkAds? secondaryAds,
+      String? adUnitSecondaryId,
+      NetworkAds? tertiaryAds,
+      String? adUnitTertiaryAdsId,
+      NetworkAds? quaternaryAds,
+      String? adUnitQuaternaryId,
+      CallbackAds? callbackAds) {
+    return FlutterAdsManagerAdmobFanApplovinPlatform.instance.showBanner(
+        context,
+        sizeBanner,
+        primaryAds,
+        adUnitPrimaryId,
+        secondaryAds,
+        adUnitSecondaryId,
+        tertiaryAds,
+        adUnitTertiaryAdsId,
+        quaternaryAds,
+        adUnitQuaternaryId,
+        callbackAds);
+  }
+
+  void showInterstitial(
+      BuildContext context,
+      NetworkAds primaryAds,
+      String adUnitPrimaryId,
+      NetworkAds? secondaryAds,
+      String? adUnitSecondaryId,
+      NetworkAds? tertiaryAds,
+      String? adUnitTertiaryAdsId,
+      NetworkAds? quaternaryAds,
+      String? adUnitQuaternaryId,
+      CallbackAds? callbackAds) {
+    return FlutterAdsManagerAdmobFanApplovinPlatform.instance.showInterstitial(
+        context,
+        primaryAds,
+        adUnitPrimaryId,
+        secondaryAds,
+        adUnitSecondaryId,
+        tertiaryAds,
+        adUnitTertiaryAdsId,
+        quaternaryAds,
+        adUnitQuaternaryId,
+        callbackAds);
+  }
+
+  void showNativeAds(
+      BuildContext context,
+      SizeNative sizeNative,
+      NetworkAds primaryAds,
+      String adUnitPrimaryId,
+      NetworkAds? secondaryAds,
+      String? adUnitSecondaryId,
+      NetworkAds? tertiaryAds,
+      String? adUnitTertiaryAdsId,
+      NetworkAds? quaternaryAds,
+      String? adUnitQuaternaryId,
+      CallbackAds? callbackAds) {
+    return FlutterAdsManagerAdmobFanApplovinPlatform.instance.showNativeAds(
+        context,
+        sizeNative,
+        primaryAds,
+        adUnitPrimaryId,
+        secondaryAds,
+        adUnitSecondaryId,
+        tertiaryAds,
+        adUnitTertiaryAdsId,
+        quaternaryAds,
+        adUnitQuaternaryId,
+        callbackAds);
+  }
+
+  void showRewards(
+      BuildContext context,
+      NetworkAds primaryAds,
+      String adUnitPrimaryId,
+      NetworkAds? secondaryAds,
+      String? adUnitSecondaryId,
+      NetworkAds? tertiaryAds,
+      String? adUnitTertiaryAdsId,
+      NetworkAds? quaternaryAds,
+      String? adUnitQuaternaryId,
+      CallbackAds? callbackAds,
+      IRewards? iRewards) {
+    return FlutterAdsManagerAdmobFanApplovinPlatform.instance.showRewards(
+        context,
+        primaryAds,
+        adUnitPrimaryId,
+        secondaryAds,
+        adUnitSecondaryId,
+        tertiaryAds,
+        adUnitTertiaryAdsId,
+        quaternaryAds,
+        adUnitQuaternaryId,
+        callbackAds,
+        iRewards);
   }
 }
--- test/flutter_ads_manager_admob_fan_applovin_test.dart
+++ test/flutter_ads_manager_admob_fan_applovin_test.dart
@@ -1,3 +1,9 @@
+import 'package:flutter_core_ads_manager/size_ads.dart';
+import 'package:flutter_core_ads_manager/network_ads.dart';
+import 'package:flutter_core_ads_manager/iadsmanager/i_rewards.dart';
+import 'package:flutter_core_ads_manager/iadsmanager/i_initialize.dart';
+import 'package:flutter_core_ads_manager/callback_ads.dart';
+import 'package:flutter/src/widgets/framework.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:flutter_ads_manager_admob_fan_applovin/flutter_ads_manager_admob_fan_applovin.dart';
 import 'package:flutter_ads_manager_admob_fan_applovin/flutter_ads_manager_admob_fan_applovin_platform_interface.dart';
@@ -7,23 +13,157 @@ import 'package:plugin_platform_interface/plugin_platform_interface.dart';
 class MockFlutterAdsManagerAdmobFanApplovinPlatform
     with MockPlatformInterfaceMixin
     implements FlutterAdsManagerAdmobFanApplovinPlatform {
-
   @override
   Future<String?> getPlatformVersion() => Future.value('42');
+
+  @override
+  void initialize(
+      BuildContext context,
+      IInitialize iInitialize,
+      NetworkAds primaryAds,
+      String? primaryAppId,
+      NetworkAds? secondaryAds,
+      String? secondaryAppId,
+      NetworkAds? tertiaryAds,
+      String? tertiaryAppId,
+      NetworkAds? quaternaryAds,
+      String? quaternaryAppId) {
+    // TODO: implement initialize
+  }
+
+  @override
+  void loadGdpr(
+      BuildContext context,
+      bool childDirected,
+      NetworkAds primaryAds,
+      NetworkAds? secondaryAds,
+      NetworkAds? tertiaryAds,
+      NetworkAds? quaternaryAds) {
+    // TODO: implement loadGdpr
+  }
+
+  @override
+  void loadInterstitial(
+      BuildContext context,
+      NetworkAds primaryAds,
+      String adUnitPrimaryId,
+      NetworkAds? secondaryAds,
+      String? adUnitSecondaryId,
+      NetworkAds? tertiaryAds,
+      String? adUnitTertiaryAdsId,
+      NetworkAds? quaternaryAds,
+      String? adUnitQuaternaryId) {
+    // TODO: implement loadInterstitial
+  }
+
+  @override
+  void loadRewards(
+      BuildContext context,
+      NetworkAds primaryAds,
+      String adUnitPrimaryId,
+      NetworkAds? secondaryAds,
+      String? adUnitSecondaryId,
+      NetworkAds? tertiaryAds,
+      String? adUnitTertiaryAdsId,
+      NetworkAds? quaternaryAds,
+      String? adUnitQuaternaryId) {
+    // TODO: implement loadRewards
+  }
+
+  @override
+  void setTestDevices(
+      BuildContext context,
+      List<String> testDevices,
+      NetworkAds primaryAds,
+      NetworkAds? secondaryAds,
+      NetworkAds? tertiaryAds,
+      NetworkAds? quaternaryAds) {
+    // TODO: implement setTestDevices
+  }
+
+  @override
+  void showBanner(
+      BuildContext context,
+      SizeBanner sizeBanner,
+      NetworkAds primaryAds,
+      String adUnitPrimaryId,
+      NetworkAds? secondaryAds,
+      String? adUnitSecondaryId,
+      NetworkAds? tertiaryAds,
+      String? adUnitTertiaryAdsId,
+      NetworkAds? quaternaryAds,
+      String? adUnitQuaternaryId,
+      CallbackAds? callbackAds) {
+    // TODO: implement showBanner
+  }
+
+  @override
+  void showInterstitial(
+      BuildContext context,
+      NetworkAds primaryAds,
+      String adUnitPrimaryId,
+      NetworkAds? secondaryAds,
+      String? adUnitSecondaryId,
+      NetworkAds? tertiaryAds,
+      String? adUnitTertiaryAdsId,
+      NetworkAds? quaternaryAds,
+      String? adUnitQuaternaryId,
+      CallbackAds? callbackAds) {
+    // TODO: implement showInterstitial
+  }
+
+  @override
+  void showNativeAds(
+      BuildContext context,
+      SizeNative sizeNative,
+      NetworkAds primaryAds,
+      String adUnitPrimaryId,
+      NetworkAds? secondaryAds,
+      String? adUnitSecondaryId,
+      NetworkAds? tertiaryAds,
+      String? adUnitTertiaryAdsId,
+      NetworkAds? quaternaryAds,
+      String? adUnitQuaternaryId,
+      CallbackAds? callbackAds) {
+    // TODO: implement showNativeAds
+  }
+
+  @override
+  void showRewards(
+      BuildContext context,
+      NetworkAds primaryAds,
+      String adUnitPrimaryId,
+      NetworkAds? secondaryAds,
+      String? adUnitSecondaryId,
+      NetworkAds? tertiaryAds,
+      String? adUnitTertiaryAdsId,
+      NetworkAds? quaternaryAds,
+      String? adUnitQuaternaryId,
+      CallbackAds? callbackAds,
+      IRewards? iRewards) {
+    // TODO: implement showRewards
+  }
 }
 
 void main() {
-  final FlutterAdsManagerAdmobFanApplovinPlatform initialPlatform = FlutterAdsManagerAdmobFanApplovinPlatform.instance;
+  final FlutterAdsManagerAdmobFanApplovinPlatform initialPlatform =
+      FlutterAdsManagerAdmobFanApplovinPlatform.instance;
 
-  test('$MethodChannelFlutterAdsManagerAdmobFanApplovin is the default instance', () {
-    expect(initialPlatform, isInstanceOf<MethodChannelFlutterAdsManagerAdmobFanApplovin>());
+  test(
+      '$MethodChannelFlutterAdsManagerAdmobFanApplovin is the default instance',
+      () {
+    expect(initialPlatform,
+        isInstanceOf<MethodChannelFlutterAdsManagerAdmobFanApplovin>());
   });
 
   test('getPlatformVersion', () async {
-    FlutterAdsManagerAdmobFanApplovin flutterAdsManagerAdmobFanApplovinPlugin = FlutterAdsManagerAdmobFanApplovin();
-    MockFlutterAdsManagerAdmobFanApplovinPlatform fakePlatform = MockFlutterAdsManagerAdmobFanApplovinPlatform();
+    FlutterAdsManagerAdmobFanApplovin flutterAdsManagerAdmobFanApplovinPlugin =
+        FlutterAdsManagerAdmobFanApplovin();
+    MockFlutterAdsManagerAdmobFanApplovinPlatform fakePlatform =
+        MockFlutterAdsManagerAdmobFanApplovinPlatform();
     FlutterAdsManagerAdmobFanApplovinPlatform.instance = fakePlatform;
 
-    expect(await flutterAdsManagerAdmobFanApplovinPlugin.getPlatformVersion(), '42');
+    expect(await flutterAdsManagerAdmobFanApplovinPlugin.getPlatformVersion(),
+        '42');
   });
 }
